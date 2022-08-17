f5a86547642fbe7c9df0cb035722e28f69f8ff1e
Yeliz Erdogan <yelizerdogan1988@gmail.com>
2022-08-17 16:32:26 +0300

    yeliz1

--- TeamCalisma.java
+++ TeamCalisma.java
@@ -2,6 +2,7 @@ public class TeamCalisma {
     public static void main(String[] args) {
         System.out.println("Team 2 Collaborate");
         System.out.println("Team 2 esra");
+        System.out.println("Team 2 yeliz");
 
     }
 }
